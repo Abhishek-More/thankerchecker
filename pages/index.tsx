8466dfc2fdf14c2b1f59bbbed5fdba7f546f876c
Abhishek More <abhimore74@gmail.com>
2023-11-16 13:59:21 -0600

    switch to ssr

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,34 +29,32 @@ const users = [
   "Nico",
 ];
 
-export default function Home() {
-  const [userInfo, setUserInfo] = useState<{ [key: string]: string }>({});
-
-  useEffect(() => {
-    let info: { [key: string]: string } = {};
-    users.forEach((user) => {
-      // GET request for remote image in node.js
-      axios({
-        method: "get",
-        url: `https://th-thanks.vercel.app/user/getinfo/${user}`,
-      }).then(function (response) {
-        info[response.data.name] = response.data.postCount;
-        setUserInfo(info);
-      });
-    });
-    console.log(info);
-  }, []);
-
+export default function Home({ info }: any) {
   return (
     <div>
       <p>Name: Thank Yous Completed </p>
-      {Object.entries(userInfo).map(([key]) => (
+      <p>---------------------------</p>
+      {Object.entries(info).map(([key]) => (
         <div key={key}>
           <p>
-            {key}:{userInfo[key] || 0}
+            {key}:{info[key] || 0}
           </p>
         </div>
       ))}
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  let info: { [key: string]: string } = {};
+  for (const user of users) {
+    let res = await axios({
+      method: "get",
+      url: `https://th-thanks.vercel.app/user/getinfo/${user}`,
+    });
+    info[res.data.name] = res.data.postCount;
+    console.log(info);
+  }
+  console.log("HELLO");
+  return { props: { info } };
+};
